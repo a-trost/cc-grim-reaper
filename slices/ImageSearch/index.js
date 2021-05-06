2fb5340b40b1139756fa7d8cbddff6669a8f9ee3
Alex Trost <alexrtrost@gmail.com>
2021-05-06 10:25:33 -0400

    Small style updates

--- slices/ImageSearch/index.js
+++ slices/ImageSearch/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
 
 const createSpeechRecognition = (onResult) => {
   if (typeof window === "undefined") {
@@ -38,14 +39,18 @@ const MySlice = ({ slice }) => {
 
   const fetchUnsplashImages = async (e, search) => {
     if (e) e.preventDefault();
+    try {
+      const res = await fetch(`/api/unsplash/${searchText || search}`);
+      const json = await res.json();
 
-    const res = await fetch(`/api/unsplash/${searchText || search}`);
-    const json = await res.json();
-
-    setImages(json);
+      setImages(json);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const onSpeechRecognitionResult = (e) => {
+    setIsRecording(false);
     setSearchText(e.results[e.results.length - 1][0].transcript);
     fetchUnsplashImages(null, e.results[e.results.length - 1][0].transcript);
   };
@@ -56,32 +61,48 @@ const MySlice = ({ slice }) => {
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
-        <h2 className="mb-20 text-5xl font-medium text-center text-red-900 font-display title-font sm:text-6xl">
+        <h2 className="mb-16 text-5xl font-medium text-center text-red-900 font-display title-font sm:text-6xl">
           {slice.primary.title}
         </h2>
-        <form action="" onSubmit={fetchUnsplashImages}>
+        <form
+          action=""
+          className="flex flex-col items-center mb-10"
+          onSubmit={fetchUnsplashImages}
+        >
+          <button
+            type="button"
+            className="flex items-center px-4 py-2 mb-4 text-red-900 bg-gray-100 border rounded-lg"
+            onClick={toggleRecording}
+          >
+            {isRecording ? (
+              <>
+                <FaMicrophoneSlash className="mr-2" /> Stop
+              </>
+            ) : (
+              <>
+                <FaMicrophone className="mr-2" /> Start
+              </>
+            )}{" "}
+            Recording
+          </button>
           <input
-            className="block w-full border"
+            className="block w-full max-w-xl px-4 py-2 mx-auto mb-4 font-bold text-center border rounded-lg font-lg font-body"
             type="text"
             id="search"
             value={searchText}
             onChange={(e) => setSearchText(e.currentTarget.value)}
           />
-          <button
-            type="button"
-            className="block border"
-            onClick={toggleRecording}
-          >
-            {isRecording ? "Stop" : "Start"} Recording
+
+          <button className="flex items-center px-4 py-2 text-red-900 bg-gray-100 border rounded-lg">
+            Search
           </button>
-          <button className="block border">Submit</button>
         </form>
         <ul className="grid grid-cols-3 gap-4">
           {images.map((image) => {
             return (
-              <li className="h-full ">
+              <li className="h-full " key={image.id}>
                 <img
-                  class="object-cover w-full h-full"
+                  className="object-cover w-full h-full"
                   src={image.urls.small}
                   alt={image.alt_description}
                 />
