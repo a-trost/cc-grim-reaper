df78fe388eb9d21424f727dfe3968cf3cbb7ab8e
Alex Trost <alexrtrost@gmail.com>
2021-05-12 11:52:01 -0400

    Make storybook fullscreen

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
 import "!style-loader!css-loader!sass-loader!/styles/styles.scss";
 import "tailwindcss/tailwind.css";
 import "@fontsource/freckle-face";
+
+export const parameters = {
+  layout: "fullscreen",
+};
