04931bf39824f978457b4616f1f11c76fdc9dc37
Alex Trost <alexrtrost@gmail.com>
2021-05-12 20:53:10 -0400

    Add Jamie's Car Slider Component

--- slices/CarSlider/index.js
+++ slices/CarSlider/index.js
@@ -1,51 +1,84 @@
 import React, { useState } from "react";
-import { RichText } from "prismic-reactjs";
+import Credit from "../../components/Credit";
 
 const MySlice = ({ slice }) => {
-  const [index, setIndex] = useState(0);
   return (
-    <section className="text-gray-600 body-font">
-      <div className="container flex flex-col flex-wrap px-5 py-24 mx-auto">
-        <div className="flex flex-wrap mx-auto mb-20">
-          {slice?.items?.map((item, i) => (
-            <button
-              key={i}
-              onClick={() => setIndex(i)}
-              className="inline-flex items-center justify-center w-1/2 py-3 font-medium leading-none tracking-wider text-indigo-500 bg-gray-100 border-b-2 border-indigo-500 rounded-t sm:px-6 sm:w-auto sm:justify-start title-font"
-            >
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                className="w-5 h-5 mr-3"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
-              </svg>
-              Car {i + 1}
-            </button>
-          ))}
-        </div>
+    <section className="relative p-12 overflow-hidden text-white bg-red-800 car-slider font-display lg:py-40 lg:px-0">
+      <div className="relative w-full m-auto md:max-w-6xl h-96">
         {slice?.items?.map((item, i) => (
-          <div className={index === i ? "block" : "hidden"}>
-            <img
-              className="block object-cover object-center w-2/3 mx-auto mb-10 rounded xl:w-1/4 lg:w-1/3 md:w-1/2"
-              alt={item.carName}
-              src={item.image.url}
-            />
-            <div className="flex flex-col w-full text-center">
-              <h1 className="mb-4 text-xl font-medium text-gray-900 title-font">
-                {item.carName}
-              </h1>
-              <p className="mx-auto text-base leading-relaxed lg:w-2/3">
-                {item.tagline}
-              </p>
+          <React.Fragment key={`car-${i}`}>
+            <input type="radio" name="trigger" id={`car-${i}`} checked />
+            {/* Slide */}
+            <div className="absolute w-full h-full">
+              {i > 0 && (
+                <label
+                  htmlFor={`car-${i - 1}`}
+                  tabindex="0"
+                  tabindex="0"
+                  className="absolute left-0 text-6xl transition transform -translate-y-1/2 opacity-50 cursor-pointer focus:ring-2 focus:ring-red-100 hover:opacity-100 top-1/2"
+                >
+                  {"<"}
+                </label>
+              )}
+              <div className="absolute top-0 left-0 transform title -rotate-6">
+                <h2 className="text-5xl uppercase lg:text-7xl">
+                  {item.carName}
+                </h2>
+                <h3 className="relative text-xl text-gray-900 opacity-75 lg:text-2xl left-12">
+                  {item.tagline}
+                </h3>
+              </div>
+              <div className="absolute left-0 right-0 w-3/4 m-auto transform -translate-y-1/2 lg:w-1/3 md:w-1/2 car top-1/2">
+                <img src={item.image.url} alt={item.carName} />
+                <img
+                  class="wheel wheel--back "
+                  src="https://assets.codepen.io/217233/grimwheel.svg"
+                />
+                <img
+                  class="wheel wheel--front"
+                  src="https://assets.codepen.io/217233/grimwheel.svg"
+                />
+                <div class="smoke">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+              </div>
+              <div className="absolute bottom-0 right-0 text-right transform price -rotate-6">
+                <p className="text-2xl text-gray-900 opacity-75 lg:text-4xl">
+                  Just
+                </p>
+                <h3 className="text-4xl lg:text-7xl">${item.price}</h3>
+                <button className="px-6 py-2 mt-2 text-red-900 transition-transform duration-200 transform bg-white rounded-lg hover:scale-110 focus:ring-2 focus:ring-blue-600">
+                  Trade In For Soul
+                </button>
+              </div>
+              {i < slice.items.length - 1 && (
+                <label
+                  htmlFor={`car-${i + 1}`}
+                  tabindex="0"
+                  className="absolute right-0 text-6xl transition transform -translate-y-1/2 opacity-50 cursor-pointer hover:opacity-100 top-1/2 focus:ring-2 focus:ring-red-100"
+                >
+                  {">"}
+                </label>
+              )}
             </div>
-          </div>
+            {/* Slide End */}
+          </React.Fragment>
         ))}
+        <div class="shadow absolute left-0 right-0 transform -translate-y-1/2 m-auto"></div>
       </div>
+      <Credit
+        author="Jamie Coulter"
+        twitter="https://twitter.com/jamiecoulter89"
+      />
     </section>
   );
 };
