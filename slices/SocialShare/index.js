1afd36f2ffb4d2a842095154b88813828f6aa8ce
Alex Trost <alexrtrost@gmail.com>
2021-05-13 10:07:22 -0400

    Update component styles

--- slices/SocialShare/index.js
+++ slices/SocialShare/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { RichText } from "prismic-reactjs";
+import Credit from "../../components/Credit";
 
 const MySlice = ({ slice }) => (
-  <section className="text-gray-600 body-font">
-    <div className="container flex flex-wrap items-center px-5 py-24 mx-auto">
+  <section className="relative text-gray-600 body-font">
+    <div className="container flex flex-wrap items-center px-5 py-24 mx-auto max-w-7xl">
       <div className="pr-0 lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0">
         <h1 className="text-3xl font-medium text-gray-900 title-font">
           {slice.primary.title}
@@ -27,7 +28,7 @@ const MySlice = ({ slice }) => (
             type="text"
             id="full-name"
             name="full-name"
-            className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
+            className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-red-500 focus:ring-2 focus:ring-red-200"
           />
         </div>
         <div className="relative mb-4">
@@ -38,17 +39,23 @@ const MySlice = ({ slice }) => (
             type="email"
             id="email"
             name="email"
-            className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200"
+            className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-white border border-gray-300 rounded outline-none focus:border-red-500 focus:ring-2 focus:ring-red-200"
           />
         </div>
-        <button className="px-8 py-2 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">
-          Button
+        <button className="px-8 py-2 text-lg text-white bg-red-500 border-0 rounded focus:outline-none hover:bg-red-600">
+          Share!
         </button>
         <p className="mt-3 text-xs text-gray-500">
-          Literally you probably haven't heard of them jean shorts.
+          By pressing this button you agree to go peacefully when the reaper
+          comes.
         </p>
       </div>
     </div>
+    <Credit
+      author="Darin Senneff"
+      twitter="https://www.twitter.com/dsenneff"
+      pending
+    />
   </section>
 );
 
